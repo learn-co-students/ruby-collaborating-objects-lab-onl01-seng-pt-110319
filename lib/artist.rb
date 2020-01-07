e93704d9f16abf8276d96361af6f30466a99d126
arnie b <arnold.bwaila@gmail.com>
2020-01-07 15:24:29 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,20 @@
+class Artist 
+  attr_accessor :name 
+  @@all = []
+  def initialize(name)
+    @name = name
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def add_song(song)
+    Song.artist = self
+  end
+  
+  def songs 
+    Song.all.select {|song| song.artist = self}
+  end
+end
+    
